c2530fcd2fe220f21bf76278b56fdbb24f2e33aa
unknown <schmidt1519@gmail.com>
2021-06-24 11:28:49 -0500

    Updated index.js to render App. Updated App.js and made App a class component. Added axios get requests for all comments and all replies. Added test youtube video embedding with iframe tag.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,54 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+      this.state = {
+        comments: [],
+        replies: [],
+      }
   }
   
-  
+  componentDidMount() {
+    this.getComments();
+    this.getReplies();
+  }
+
+  getComments = async () => {
+    try{
+      let response = await axios.get(`http://127.0.0.1:8000/comments/`)
+      this.setState({
+        comments: response.data,
+      });
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
+  getReplies = async () => {
+    try{
+      let response = await axios.get(`http://127.0.0.1:8000/reply/`)
+      this.setState({
+        replies: response.data,
+      });
+    }
+    catch (err) {
+      console.log(err)
+    }
+  }
+
   render() { 
     return (
       <div>
         <h1>test</h1>
+
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+  src="https://www.youtube.com/embed/M7lc1UVf-VE?autoplay=1&origin=http://example.com"
+  frameborder="0"></iframe>
+
       </div>
     );
   }
